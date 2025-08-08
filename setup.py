85a26aabcbf8ca73c15ebf514eddc7599ad0e4d2
JWZero <J.Wankmueller@campus.lmu.de>
2025-08-08 10:35:00 +0200

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -1,48 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# This is a minimal setup.py that defers to pyproject.toml
+# All package configuration should be in pyproject.toml
 
-setup(
-    name="ordinal_xai",
-    version="0.2.0",
-    author="Jakob Wankmüller",
-    author_email="crjakobcr@gmail.com",
-    description="A Python package for ordinal regression and model-agnostic interpretation methods",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/JWzero/ordinal_xai",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'ordinal_xai=ordinal_xai.__main__:main',
-        ],
-    },
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.12",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
-    python_requires=">=3.8",
-    install_requires=[
-        "numpy",
-        "pandas",
-        "scikit-learn",
-        "scipy",
-        "matplotlib",
-        "gower",
-        "statsmodels",
-        "xgboost",
-        "torch",
-        "skorch",
-        "dlordinal",
-    ],
-    include_package_data=True,
-    package_data={
-        "ordinal_xai": ["data/*.csv"],
-    },
-) 
+if __name__ == "__main__":
+    setup()
